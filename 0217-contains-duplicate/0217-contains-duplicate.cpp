9445f7df2839174435144cca5eb66e62709cde2a
KulishDadhich <kulishdadhich37@gmail.com>
2026-07-15 12:22:43 +0530

    Time: 89 ms (12.37%), Space: 113.5 MB (6.15%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,11 +3,13 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
 
-        sort(nums.begin() , nums.end());
-        for(int i = 0;i<n - 1;i++){
-            if(nums[i] == nums[i +1]){
-                return true;
-            }
+        unordered_map<int , int> freq;
+        for(int x:nums){
+            freq[x]++;
+
+        }
+        for(int i = 0;i<n;i++){
+            if(freq[nums[i]] >= 2) return true;
         }
         return false;
 
